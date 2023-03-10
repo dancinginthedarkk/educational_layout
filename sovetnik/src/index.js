3f3cdd05a732629eb4b2685b91ccb45ebab6c09f
dancinginthedarkk <m.angellina@yandex.ru>
2023-03-10 13:30:47 +0300

    add server.js and remove webpack, organized pictures into folders

--- sovetnik/src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles/index.css';
-import './index.html';
-import './post.html';
-import './styles/post.css';
---
+++ sovetnik/src/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('.'));
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
+});
+
+app.get('/post', (req,res) => {
+    res.sendFile(path.join(__dirname, '.', 'post.html'));
+});
+
+app.listen(port, function() {
+    console.log(`Server listening port ${port}`);
+});
